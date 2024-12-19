c47989e175c87d45eb631573ca0400d30648f282
Andres Oficina <andresgam31@gmail.com>
2024-12-19 14:18:40 -0500

    actualizacion

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
 import imagenRickMorty from "./img/rick-morty.png";
 import "./App.css";
+import { useState } from "react";
+import Caracteres from "./componentes/caracteres";
 
 function App() {
-  const reqApi = () => {
-    console.log("clickimg");
+  const [characters, setCharacteres] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+    // console.log(characterApi);
+
+    setCharacteres(characterApi.results);
   };
+
+  // console.log(characters);
 
   return (
     <div className="App">
       <header className="App-header">
         <h1 className="title">Rick & Morty</h1>
-        <img src={imagenRickMorty} alt="Rick & Morty" className="img-home" />
-        <button onClick={reqApi} className="btn-search">
-          Buscar Personajes
-        </button>
+        {characters ? (
+          <Caracteres Caracteres={characters} />
+        ) : (
+          <>
+            <img
+              src={imagenRickMorty}
+              alt="Rick & Morty"
+              className="img-home"
+            />
+            <button onClick={reqApi} className="btn-search">
+              Buscar Personajes
+            </button>
+          </>
+        )}
       </header>
     </div>
   );
